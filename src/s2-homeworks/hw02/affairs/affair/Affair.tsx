793a31c5207c5f4ca323ddf98a7bff283a611785
Baykov Aleksandr <baykov-1988@mail.ru>
2025-10-18 16:19:20 +0300

    implemented filtering and deleting of fields

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -4,14 +4,15 @@ import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: AffairType
-    deleteAffairCallback: any // need to fix any
-}
+  // key не нужно типизировать
+  affair: AffairType;
+  deleteAffairCallback: (_id: number) => void; // need to fix any
+};
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
         // need to fix
+        props.deleteAffairCallback(props.affair._id)
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -19,31 +20,26 @@ function Affair(props: AffairPropsType) {
     const affairClass = s.affair + ' ' + s2[props.affair.priority]
 
     return (
-        <div
-            id={'hw2-affair-' + props.affair._id}
-            className={affairClass}
-        >
-            <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
+      <div id={"hw2-affair-" + props.affair._id} className={affairClass}>
+        <div id={"hw2-name-" + props.affair._id} className={nameClass}>
+          {/*создаёт студент*/}
 
-                {/**/}
-            </div>
-            <div id={'hw2-priority-' + props.affair._id} hidden>
-                {props.affair.priority}
-            </div>
-
-            <button
-                id={'hw2-button-delete-' + props.affair._id}
-                className={buttonClass}
-                // need to fix
-
-            >
-                {/*текст кнопки могут изменить студенты*/}
-                X
-                {/**/}
-            </button>
+          {/**/}
         </div>
-    )
+        <div id={"hw2-priority-" + props.affair._id} hidden>
+          {props.affair.priority}
+        </div>
+
+        <button
+          id={"hw2-button-delete-" + props.affair._id}
+          className={buttonClass}
+          // need to fix
+          onClick={deleteCallback}
+        >
+          {/*текст кнопки могут изменить студенты*/}X{/**/}
+        </button>
+      </div>
+    );
 }
 
 export default Affair
